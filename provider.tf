e2cd546e3d45c758b4b331f88553a7ea35521a3f
shiva270597 <enugalareddy.er@gmail.com>
2024-12-02 21:52:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
