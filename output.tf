9fe779ee539f8a4030cd17d7988fdc70c9df8cab
shiva270597 <enugalareddy.er@gmail.com>
2024-12-02 16:58:42 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+  description = "ID of the VPC"
+}
+
+output "subnet_id" {
+  value = aws_subnet.main_subnet.id
+  description = "ID of the Subnet"
+}
+
+output "web_server_public_ip" {
+  value = aws_instance.web_server.public_ip
+  description = "Public IP of the Web Server"
+}
