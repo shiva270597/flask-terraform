3b5dc4f1e2d4d5fb227671c6f79cc57ec1eada90
shiva270597 <enugalareddy.er@gmail.com>
2024-12-02 22:12:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.78.0"
     }
   }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
