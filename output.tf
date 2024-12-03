160bdf71df13b34f2d3039fbbd4cd3aa505f5b35
shiva270597 <enugalareddy.er@gmail.com>
2024-12-03 19:42:46 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,6 +9,6 @@ output "subnet_id" {
 }
 
 output "web_server2_public_ip" {
-  value = aws_instance.web_server.public_ip
+  value = aws_instance.web_server2.public_ip
   description = "Public IP of the Web Server"
 }
